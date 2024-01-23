d5ea8cf9f1126414b3aeb85918a4a87edd4993ce
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2024-01-23 20:32:05 +0300

    feat: custom style & Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./index.css";
+import "./style.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home.tsx";
+import Home from "./pages/Home";
 
 function App() {
   return (
